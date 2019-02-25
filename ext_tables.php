132196e7b8ec6029a9b41a7165dae94b7025a6ee
Claus Due <claus@Clauss-Mac-Pro.mydomain>
2019-02-25 23:22:46 +0100

    [TASK] Update to Flux master compatibility

--- ext_tables.php
+++ ext_tables.php
@@ -3,4 +3,4 @@ defined('TYPO3_MODE') or die ('Access denied.');
 
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile($_EXTKEY, 'Configuration/TypoScript', 'My provider extension');
 
-\TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerPlugin('Test.Testing', 'Test', 'Test plugin');
+\TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerPlugin('FluidTYPO3.TestProviderExtension', 'Test', 'Test plugin');
