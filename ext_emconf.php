dbeac0d65250a36790654a4e5434aebdb4308047
Claus Due <claus@namelesscoder.net>
2020-05-01 12:07:12 +0200

    [TASK] Declare compatible with TYPO3v10.4

--- ext_emconf.php
+++ ext_emconf.php
@@ -34,7 +34,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'CGLcompliance_note' => '',
 	'constraints' => array(
 		'depends' => array(
-			'typo3' => '4.5-10.1.99',
+			'typo3' => '4.5-10.4.99',
 			'flux' => '',
 			'vhs' => '',
 		),
