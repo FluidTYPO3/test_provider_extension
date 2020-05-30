222f7f6b8c5e319dc6d3fae696d2b668dadc8fe5
Claus Due <claus@namelesscoder.net>
2020-05-30 17:28:31 +0200

    [TASK] Update templates, add controller examples

---
+++ Classes/Controller/ContentController.php
@@ -0,0 +1,26 @@
+<?php
+namespace FluidTYPO3\TestProviderExtension\Controller;
+
+use FluidTYPO3\Flux\Controller\AbstractFluxController;
+
+/**
+ * Class ContentController
+ */
+class ContentController extends AbstractFluxController
+{
+    /**
+     * @return void
+     */
+    public function sectionObjectAsGridAction()
+    {
+        #die('test');
+    }
+
+    /**
+     * @return void
+     */
+    public function columnsAction()
+    {
+        #die('test');
+    }
+}
---
+++ Classes/Controller/PageController.php
@@ -0,0 +1,16 @@
+<?php
+namespace FluidTYPO3\TestProviderExtension\Controller;
+
+/**
+ * Class PageController
+ */
+class PageController extends \FluidTYPO3\Flux\Controller\PageController
+{
+    /**
+     * @return void
+     */
+    public function standardAction()
+    {
+
+    }
+}
