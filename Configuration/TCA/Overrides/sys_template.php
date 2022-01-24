ff09df659e60e748559ceb32ddb9c50ef5079c15
Christian Weiske <weiske@mogic.com>
2022-01-24 17:23:52 +0100

    Remove TCA modifications from ext_tables.php
    .. this makes the "Check TCA in ext_tables.php" upgrade wizard happy

--- Configuration/TCA/Overrides/sys_template.php
+++ Configuration/TCA/Overrides/sys_template.php
@@ -2,5 +2,3 @@
 defined('TYPO3_MODE') or die ('Access denied.');
 
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile('test_provider_extension', 'Configuration/TypoScript', 'My provider extension');
-
-\TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerPlugin('FluidTYPO3.TestProviderExtension', 'Test', 'Test plugin');
---
+++ Configuration/TCA/Overrides/tt_content.php
@@ -0,0 +1,4 @@
+<?php
+defined('TYPO3_MODE') or die ('Access denied.');
+
+\TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerPlugin('FluidTYPO3.TestProviderExtension', 'Test', 'Test plugin');
